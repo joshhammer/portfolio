9131c63a052c206be09fa6cca9fa979fc736abd2
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-03-21 14:16:45 +0100

    make some changes for gh-pages to work, use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Landingpage from './components/LandingPage/LandingPage'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
