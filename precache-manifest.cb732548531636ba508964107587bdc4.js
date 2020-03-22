6b0b61c67d1105ba0cdda92e3474380adc77b0bd
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2020-03-22 10:08:16 +0100

    Updates

---
+++ precache-manifest.cb732548531636ba508964107587bdc4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bb29f69f341ea2c82620428e8f0e59",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "efd91b549432dcde90ef",
+    "url": "/portfolio/static/css/main.133cc6bd.chunk.css"
+  },
+  {
+    "revision": "4d8049b04fb40c55af40",
+    "url": "/portfolio/static/js/2.3c941295.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/portfolio/static/js/2.3c941295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd91b549432dcde90ef",
+    "url": "/portfolio/static/js/main.d5575323.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "39b7fbffa5ac74d62320589b1492d46d",
+    "url": "/portfolio/static/media/etch-a-sketch.39b7fbff.gif"
+  },
+  {
+    "revision": "f4bdcec7b61485e954959231abad7729",
+    "url": "/portfolio/static/media/farlow.f4bdcec7.jpg"
+  },
+  {
+    "revision": "440e007175817e00c337701adabb2ff4",
+    "url": "/portfolio/static/media/josh-hammer.440e0071.jpg"
+  },
+  {
+    "revision": "1b3558ddddc008222739b046ce382280",
+    "url": "/portfolio/static/media/razzpay.1b3558dd.png"
+  },
+  {
+    "revision": "23fb83c7398b5dfab89c95a58327b517",
+    "url": "/portfolio/static/media/rgb-guessr.23fb83c7.gif"
+  },
+  {
+    "revision": "234f45f199a4aeda42b807db813d2ff4",
+    "url": "/portfolio/static/media/rock-paper.234f45f1.gif"
+  },
+  {
+    "revision": "12f0f78b6def89c721de5458a628b181",
+    "url": "/portfolio/static/media/shooter.12f0f78b.gif"
+  },
+  {
+    "revision": "93bf285b19585e0a7f6d68bb90675a2c",
+    "url": "/portfolio/static/media/synthpop.93bf285b.png"
+  },
+  {
+    "revision": "6032d22b260e558a9eaa1e56d0ed4019",
+    "url": "/portfolio/static/media/todo.6032d22b.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.15604862a716322f51d4d8460027631e.js"
+  "/portfolio/precache-manifest.cb732548531636ba508964107587bdc4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
